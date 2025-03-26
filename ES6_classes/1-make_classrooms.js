9074fa823c2b6e3ccfface17a18926fcd1ab29bc
MINS2405 <9963@holbertonstudents.com>
2025-03-26 16:08:15 +0100

    ES6 Basics

--- ES6_classes/1-make_classrooms.js
+++ ES6_classes/1-make_classrooms.js
@@ -1,12 +1,10 @@
-import ClassRoom from './0-classroom';
-
 export default function taskBlock(trueOrFalse) {
-  let task = false;
-  let task2 = true;
+   let task = false;
+   let task2 = true;
 
   if (trueOrFalse) {
-      task = true;
-      task2 = false;
+   let task = true;
+   let task2 = false;
   }
 
   return [task, task2];
